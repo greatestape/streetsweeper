cd1fc8fe5dbb31a76857fb030741130f832272dc
Sam Bull <sbull@trapeze.com>
2008-09-01 15:03:36 -0400

    Moved street related models into a separate app.

--- mystreet/mosaics/models.py
+++ mystreet/mosaics/models.py
@@ -1,80 +1,33 @@
 from django.db import models
 from django.utils.translation import ugettext as _, ugettext_lazy as _lazy
 
-class StreetSide(models.Model):
-    """A side of a street, (i.e. North Side, South Side)"""
-    name = models.CharField(_lazy('name'), max_length=100)
-    initials = models.CharField(_lazy('initials'), max_length=4, unique=True)
-    
-    class Admin:
-        list_display = ('name', 'initials')
-        search_fields = ('name', 'initials')
-    
-    class Meta:
-        verbose_name = _(u'street side')
-        verbose_name_plural = _(u'street sides')
-    
-    def __unicode__(self):
-        return _(u'%(name)s') % {'name': self.name}
-
-class Street(models.Model):
-    """A street that will be depicted using mosaics"""
-    name = models.CharField(_lazy('name'), max_length=100)
-    sides = models.ManyToManyField(StreetSides, verbose_name=_lazy(u'sides'))
+class Mosaic(models.Model):
+    """A mosaic of images"""
+    name = models.CharField(blank=True, max_length=100)
 
     class Admin:
         list_display = ('name',)
         search_fields = ('name',)
 
     class Meta:
-        verbose_name = _(u'street')
-        verbose_name_plural = _(u'streets')
+        verbose_name = _(u'mosaic')
+        verbose_name_plural = _(u'mosaics')
 
     def __unicode__(self):
-        return _(u'%(name)s') % {'name': self.name}
-
-class AddressedLocation(models.Model):
-    """An addressed place on a street"""
-    address = AddressField(_lazy('address'))
-    offset = models.PositiveIntegerField(_lazy('offset'), help_text=_lazy('offset from street origin measured in metres'))
-    length = models.PositiveIntegerField(_lazy('length'), help_text=_lazy('length of this address in terms of "street-front real estate", measured in metres'))
-
-    class Admin:
-        list_display = ('',)
-        search_fields = ('',)
-
-    class Meta:
-        verbose_name = _(u'')
-
-    def __unicode__(self):
-        return u'Address'
-
-
-class Block(models.Model):
-    """A block of a city street"""
-    street = models.ForeignKey(Street, verbose_name=_lazy(u'street'))
-    side = models.CharField(_lazy('side of street'), choices=STREET_SIDE_CHOICES)
-    begin_address = models.ForeignKey()
-
-    def get_side_street_combo(self):
-        return _(u'%(side_initials)s side of %(street)s') % {'side_initials': self.side.initials, 'street': self.street.name}
-    
-
-    class Admin:
-        list_display = ('get_side_street_combo', )
-        search_fields = ('',)
-
-    class Meta:
-        verbose_name = _(u'')
-
-    def __unicode__(self):
-        return u'Block'
-
+        return _(u'%(name)') % {'name': self.name}
 
 
 class Patch(models.Model):
     """A Patch is a single image element with placement and cropping information for rendering it within a mosaic"""
-    name = models.CharField(blank=True, max_length=100)
+    mosaic = models.ForeignKey(Mosaic, verbose_name=_lazy(u'mosaic'))
+    scale = models.FloatField(_lazy('scale'), help_text=_lazy('The number of pixels in one AU (arbitrary unit) in the mosaic'))
+    position_x = models.FloatField(_lazy('position (x)'))
+    position_y = models.FloatField(_lazy('position (y)'))
+    
+    source_image = models.ForeignKey('Photo', verbose_name=_lazy(u'source image'))
+    colour_correction = models.ForeignKey('ColourCorrection', verbose_name=_lazy(u'colour correction'))
+    rotation = models.FloatField(_lazy('rotation'), help_text=_lazy('measured in radians, positive values rotate counte-clockwise'))
+    cropping = models.
 
     class Admin:
         list_display = ('',)
