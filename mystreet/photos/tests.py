54dd1b38b6e8489bd913e1515eea87e80ec090c1
Sam Bull <sbull@trapeze.com>
2009-06-08 10:53:59 -0400

    Restructured photo upload test.

--- mystreet/photos/tests.py
+++ mystreet/photos/tests.py
@@ -14,18 +14,16 @@ class PhotoUploadTestCase(TestCase):
     """Testing that photos can be uploaded"""
     def setUp(self):
         self.upload_url = reverse('photo_upload')
-        self.test_file = _get_test_photo_file()
-        self.test_name = 'Cat Photo'
-        self.response = self.client.post(self.upload_url, {'name': self.test_name, 'photo': open(self.test_file)})
 
-    def testPhotoUploadRedirectsToStatusView(self):
-        self.assertRedirects(self.response, reverse('photo_upload_status'))
-
-    def testPhotoInDB(self):
+    def testPhotoUploadPost(self):
+        test_file = _get_test_photo_file()
+        test_name = 'Cat Photo'
+        response = self.client.post(self.upload_url, {'name': test_name, 'photo': open(test_file)})
+        self.assertRedirects(response, reverse('photo_upload_status'))
         self.assertEqual(Photo.objects.count(), 1)
         new_photo = Photo.objects.all()[0]
-        actual_file = open(self.test_file)
-        actual_image = Image.open(self.test_file)
+        actual_file = open(test_file)
+        actual_image = Image.open(test_file)
         actual_width, actual_height = actual_image.size
         self.assertEqual(actual_file.read(), new_photo.photo.read())
         self.assertEqual(new_photo.width, actual_width)
