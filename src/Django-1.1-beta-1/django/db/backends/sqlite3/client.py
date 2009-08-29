082794f558f1dcfa21dadcf8cad8ebc2cb16717e
Sam Bull <sbull@trapeze.com>
2009-08-29 02:09:06 -0400

    Updated to the final release of Django 1.1

--- src/Django-1.1-beta-1/django/db/backends/sqlite3/client.py
+++
@@ -1,9 +0,0 @@
-from django.db.backends import BaseDatabaseClient
-import os
-
-class DatabaseClient(BaseDatabaseClient):
-    executable_name = 'sqlite3'
-
-    def runshell(self):
-        args = ['', self.connection.settings_dict['DATABASE_NAME']]
-        os.execvp(self.executable_name, args)
