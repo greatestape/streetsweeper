bec77dad0449d5f6a05198400e52b98fc21006e8
Sam Bull <sbull@trapeze.com>
2009-10-03 01:38:48 -0400

    Added demo homepage

--- streetsweeper/urls.py
+++ streetsweeper/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 
 
 urlpatterns = patterns('',
+    (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}, 'home'),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/(.*)', admin.site.root),
     (r'^photos/', include('photos.urls')),
