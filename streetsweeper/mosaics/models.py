535f35a0890a3fbbf73431ab8ec218be051c5750
Sam Bull <sbull@trapeze.com>
2009-10-12 15:17:00 -0400

    Removed unimplemented colour correction field.

--- streetsweeper/mosaics/models.py
+++ streetsweeper/mosaics/models.py
@@ -27,7 +27,6 @@ class Patch(models.Model):
     position_y = models.FloatField(_lazy('position (y)'))
 
     source_image = models.ForeignKey('Photo', verbose_name=_lazy(u'source image'))
-    colour_correction = models.ForeignKey('ColourCorrection', verbose_name=_lazy(u'colour correction'))
     rotation = models.FloatField(_lazy('rotation'), help_text=_lazy('measured in radians, positive values rotate counte-clockwise'))
     mask = models.CommaSeparatedIntegerField(_('mask'), blank=True, max_length=512)
     modified_image = models.ImageField(_('modified image'), upload_to=get_modified_file_path, null=True, blank=True, height_field=, width_field=)
