696a272656007f56e35be7517687f283e7e6d47b
Sam Bull <sbull@trapeze.com>
2009-10-13 10:54:21 -0400

    Updated admin URL config to Django 1.1 format.

--- streetsweeper/urls.py
+++ streetsweeper/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls.defaults import *
 from django.contrib import admin
 
 
+admin.autodiscover()
+
+
 urlpatterns = patterns('',
     (r'^$', 'home.views.home', {}, 'home'),
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/(.*)', admin.site.root),
+    (r'^admin/', include(admin.site.urls)),
     (r'^photos/', include('photos.urls')),
     (r'^accounts/', include('django.contrib.auth.urls')),
 )
