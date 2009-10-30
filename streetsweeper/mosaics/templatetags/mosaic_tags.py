eaa6783ceeb8b7a6e7dc371a9a784ad9ea47775f
Sam Bull <sbull@trapeze.com>
2009-10-30 09:36:54 -0400

    Added a zoom parameter to the mosaic template tag. Added image creation functionality.

--- streetsweeper/mosaics/templatetags/mosaic_tags.py
+++ streetsweeper/mosaics/templatetags/mosaic_tags.py
@@ -1,3 +1,7 @@
+import Image
+import os.path
+
+from django.conf import settings
 from django import template
 
 
@@ -5,12 +9,32 @@ register = template.Library()
 
 
 @register.inclusion_tag('mosaics/includes/mosaic.html')
-def show_mosaic(mosaic, width, height, edge_height, xoffset):
-    # Define the filename for the mosaic image that's going to be created
-    # If this file already exists, just return it
+def show_mosaic(mosaic, width, height, zoom, edge_height, xoffset):
+    file_path = (
+            'managed/mosaics/%(mosaic)s/'
+            '%(width)sx%(height)sx%(zoom)s-eh%(edge_height)s/'
+            'xoffset%(xoffset)s.jpg' % {
+                'mosaic': mosaic.slug,
+                'width': width,
+                'height': height,
+                'zoom': zoom,
+                'edge_height': edge_height,
+                'xoffset': xoffset
+            }
+            )
+
+    local_file_path = os.path.join(
+        settings.MEDIA_ROOT,
+        file_path
+        )
+    if(not os.path.exists(local_file_path)):
+        os.makedirs(os.path.dirname(local_file_path))
+
+        image = Image.new('RGBA', (width, height))
+        image.save(local_file_path)
+
     # TODO: gather tiles required to render area of mosaic defined by parameters
-    # Create PIL image
     # Open each tile and place it in the PIL image at the appropriate coordinates
     # Save the image to the generated filename
     # return the filename (to the template, which should generate the image tag)
-    return {}
+    return {'slice_url': '%s%s' % (settings.MEDIA_URL, file_path)}
