b32ba5d939e73c5d051335c7bc1682553aacd1f7
Sam Bull <sbull@trapeze.com>
2011-01-16 17:58:12 -0500

    Added labels and help_text to viewport form.

--- streetsweeper/home/forms.py
+++ streetsweeper/home/forms.py
@@ -1,12 +1,16 @@
 from django import forms
+from django.utils.translation import ugettext as _
 
 DEFAULT_WIDTH = 100
 DEFAULT_POSITION = 0.0
 
 
 class ViewportForm(forms.Form):
-    position = forms.FloatField(required=False)
-    width = forms.IntegerField(required=False)
+    position = forms.FloatField(label=_('position (m)'), required=False,
+            help_text=_("The position along the street upon which the "
+            "viewport should be centred."))
+    width = forms.IntegerField(label=_('width (m)'), required=False,
+            help_text=_("The width of the viewport in metres"))
 
     def clean_width(self):
         width = self.cleaned_data.get('width')
